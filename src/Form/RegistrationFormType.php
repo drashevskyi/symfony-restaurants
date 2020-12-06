b93bb3212a04f3e28a589321e0f381cfdb4e3d0c
Dima Rashevskyi <31186934+drashevskyi@users.noreply.github.com>
2020-12-07 00:02:24 +0300

    update registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,18 +17,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('email', null, [
+                'attr'=> ['class'=>'form-control']
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'attr'=> ['class'=>'form-control'],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
